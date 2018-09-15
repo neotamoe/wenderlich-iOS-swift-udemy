7bedba0512c7cfa74aed1758ba936a3effbd6f7d
Neota Moe <neota.moe@gmail.com>
2018-09-14 21:14:32 -0500

    added two view controllers

---
+++ BullsEye/BullsEye/AboutCoderViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AboutCoderViewController.swift
+//  BullsEye
+//
+//  Created by Neota Moe on 9/14/18.
+//  Copyright © 2018 Neota Moe. All rights reserved.
+//
+
+import UIKit
+
+class AboutCoderViewController: UIViewController {
+
+    @IBAction func close () {
+        dismiss(animated: true, completion: nil)
+    }
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+
+}
