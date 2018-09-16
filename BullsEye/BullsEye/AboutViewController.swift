b55c8016fe6115b19fa5536e6110565811bb8cbd
Neota Moe <neota.moe@gmail.com>
2018-09-15 20:43:39 -0500

    webkit view and auto layout

--- BullsEye/BullsEye/AboutViewController.swift
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
 
-    @IBAction func close() {
+  @IBOutlet weak var webView: WKWebView!
+  
+  @IBAction func close() {
         dismiss(animated: true, completion: nil)
     }
   
     override func viewDidLoad() {
         super.viewDidLoad()
+      if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+        if let htmlData = try? Data(contentsOf: url) {
+          let baseURL = URL(fileURLWithPath: Bundle.main.bundlePath)
+          webView.load(htmlData, mimeType: "text/html", characterEncodingName: "UTF-8", baseURL: baseURL)
+        }
+      }
 
     }
 
